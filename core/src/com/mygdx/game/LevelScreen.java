7a9ae39319b249aa8dfa444ce589a05a7abae7ba
Vaiolo <violettafazzi@gmail.com>
2023-05-04 14:42:32 +0200

    Level Screen

---
+++ core/src/com/mygdx/game/LevelScreen.java
@@ -0,0 +1,14 @@
+package com.mygdx.game;
+
+public class LevelScreen extends BaseScreen{
+    public void initialize()
+    {
+        BaseActor back = new BaseActor(0,0, mainStage);
+
+    }
+
+    public void update(float dt)
+    {
+
+    }
+}
